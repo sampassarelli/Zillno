d8692932c800e2921d3993bd5d1265f358d3d7a2
brodrickgeorge <brodrick.george@yahoo.com>
2020-12-04 16:02:56 -0600

    fixed depended destroy

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,7 +1,7 @@
 class House < ApplicationRecord
-    has_many :house_reviews
-    has_many :tours
-    has_many :offers
+    has_many :house_reviews, dependent: :destroy
+    has_many :tours, dependent: :destroy
+    has_many :offers, dependent: :destroy
 
     belongs_to :seller, :class_name => "User"
     belongs_to :agent
